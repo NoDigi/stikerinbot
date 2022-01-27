26b2ed746b1a3477ad1f4827fbb41134f2ba9e02
NoDigi <98347766+NoDigi@users.noreply.github.com>
2022-01-27 16:40:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ global.APIKeys = { // APIKey nya disini
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://pencarikode.xyz': 'pais',
-  'https://api.xteam.xyz': 'apikeymu',
+  'https://api.xteam.xyz': 'f6898e891be84fcd',
   'https://api.zeks.xyz': 'apivinz',
 }
 
